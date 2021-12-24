2f3425bec75f97df099c0158350807f2f3a53f1c
Tom Goodsell <ymylei@users.noreply.github.com>
2021-12-24 14:43:50 -0600

    add test post and place items into group

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -9,12 +10,17 @@ import (
 
 func main() {
 	test := gin.Default()
-	test.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"testing": "testin1234",
-		})
-	})
+
 	test.GET("/healthcheck", healthCheck)
+	v1 := test.Group("v1")
+	{
+		v1.GET("/test", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"testing": "testin1234",
+			})
+		})
+		v1.POST("/testpost", testPost)
+	}
 	err := test.Run()
 	if err != nil {
 		log.Fatal(err)
@@ -25,5 +31,19 @@ func healthCheck(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"serviceStatus": "OK",
 		"hostname":      "localhost",
+	})
+}
+
+func testPost(ctx *gin.Context) {
+	name := ctx.PostForm("name")
+	value := ctx.PostForm("value")
+
+	if len(name) > 0 && len(value) > 0 {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("My name is %s and you have %s value to me,", name, value),
+		})
+	}
+	ctx.JSON(http.StatusBadRequest, gin.H{
+		"error": "you've not provided the appropriate name and value values, cheers.",
 	})
 }
